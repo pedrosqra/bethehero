a748f4a574d1a237c1e84db8b759ac93a29dfa62
Pedro <pedro.lima@ccc.ufcg.edu.br>
2020-06-05 11:35:17 -0300

    validation

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
-
+const { celebrate, Segments, Joi } = require('celebrate');
+ 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
@@ -7,15 +8,58 @@ const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
+// ROTA DE LISTAGEM DE ONGS
 routes.get('/ongs', OngController.index); 
-routes.post('/ongs', OngController.create);
 
-routes.post('/sessions', SessionController.create);
+// ROTA DE CADASTRO DE ONGAS
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required().max(13),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    },)
+}), OngController.create);
 
-routes.get('/profile', ProfileController.index);
+// ROTA DE LOGIN
+routes.post('/sessions', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        id: Joi.string().required()
+    }),
+}), SessionController.create);
 
-routes.get('/incidents', IncidentController.index); 
-routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+// ROTA DE LISTAGEM DE CASOS DE UMA ONG LOGADA
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({authorization: Joi.string().required(),
+}).unknown(),  
+}), ProfileController.index);
+
+// ROTA PÁGINA DE CASOS DE UMA ONG
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    }),
+}), IncidentController.index); 
+
+// ROTA DE CADASTRO DE INCIDENTES DE UMA ONG
+routes.post('/incidents', celebrate({
+    [Segments.HEADERS]: Joi.object({authorization: Joi.string().required(),
+    }).unknown(),
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        value: Joi.number().required(),
+    },)
+}), IncidentController.create);
+
+// ROTA PARA DELETAR UM INCIDENTE DE UM ONG
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    }),
+    [Segments.HEADERS]: Joi.object({authorization: Joi.string().required(),
+}).unknown(),
+}), IncidentController.delete);
 
 module.exports = routes;
